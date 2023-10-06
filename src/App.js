f611b90e817580704817557a88033bf991783d4a
loretari <loretari@gmail.com>
2023-10-06 19:40:19 +0300

    init: add Footer folder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Intro from './components/Intro/intro';
 import Skills from './components/Skills/skills';
 import Works from './components/Works/works';
 import Contact from './components/Contact/contact';
+import Footer from './components/Footer.'
 
 
 
